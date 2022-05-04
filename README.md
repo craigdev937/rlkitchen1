cbaaf30027231d5272d7f80d4efe12b4b3e9a735
Craig <indigomx9@gmail.com>
2022-05-04 02:09:17 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Location
+
+Initial setup of the project.  Created with TypeScript, Webpack, React, and React Location.
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea sapiente mollitia consequatur quas aspernatur corrupti laboriosam consectetur corporis, harum quibusdam perspiciatis, quisquam iusto? Iusto incidunt enim ipsum rem, doloribus accusamus?</p>
+        </React.Fragment>
+    );
+};
+
+
